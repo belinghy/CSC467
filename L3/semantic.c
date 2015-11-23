2f5092dfbe4956f0e4ac08c39510d70efaeba760
Ben <gs1679@gmail.com>
2015-11-22 22:24:25 -0500

    change errorOccurred from 1 to true

--- L3/semantic.c
+++ L3/semantic.c
@@ -255,16 +255,16 @@ int semantic_check_recurse(node *n, SymbolTable *s){
       if((n->constructor.arguments) == NULL){
         if(n->type_info->length != 0){
           fprintf(errorFile, "ERROR: line %d: 0 input arguments, expecting %d\n", n->line, n->type_info->length);
-          errorOccurred = 1;
+          errorOccurred = true;
         }
       } else {
         if(n->type_info->length != (n->constructor.arguments)->arguments.num_args){ /* Number of arguments check */
           fprintf(errorFile, "ERROR: line %d: %d input arguments, expecting %d\n", n->line, (n->constructor.arguments)->arguments.num_args, n->type_info->length);
-          errorOccurred = 1;
+          errorOccurred = true;
         } else if((n->constructor.arguments)->type_info->basic_type != n->type_info->basic_type  /* Arguments type check - ANY type is allowed*/
                   && ((n->constructor.arguments)->type_info->basic_type != Type::ANY)){
           fprintf(errorFile, "ERROR: line %d: incorrect argument type\n", n->line);
-          errorOccurred = 1;
+          errorOccurred = true;
         }
       }
       break;
@@ -295,7 +295,7 @@ int semantic_check_recurse(node *n, SymbolTable *s){
       SymAttr *attr = s->lookup(n->variable.identifier);
       if(attr == NULL){
         fprintf(errorFile, "ERROR: line %d: undeclared variable %s\n", n->line, n->variable.identifier);
-        errorOccurred = 1;
+        errorOccurred = true;
         break;
       } 
 
@@ -308,7 +308,7 @@ int semantic_check_recurse(node *n, SymbolTable *s){
       /* Checking index is not out of bounds */
       if((n->type_info)->length <= n->variable.index){
         fprintf(errorFile, "ERROR: line %d: index %d is out of bounds for variable %s of length %d\n", n->line, n->variable.index, n->variable.identifier, (n->type_info)->length);
-        errorOccurred = 1;
+        errorOccurred = true;
       }
       break;
     }
@@ -325,7 +325,7 @@ int semantic_check_recurse(node *n, SymbolTable *s){
           if(type_check((n->arguments.arguments)->type_info, (n->arguments.argument)->type_info) /* Arguments are not the same type (excepting the ANY type) */
              && (n->arguments.arguments)->type_info->basic_type != Type::ANY){
             fprintf(errorFile, "ERROR: line %d: incorrect argument type\n", n->line);
-            errorOccurred = 1;
+            errorOccurred = true;
             n->type_info->basic_type = Type::ANY; /* Use any type from now on */
             n->type_info->length = -1;
           } else {
@@ -372,13 +372,13 @@ void function_check_args(int line, int func_id, Type *ret_type, Type *arg_type, 
     case 0: //DP3
     if(num_args != 2){
       fprintf(errorFile, "ERROR: line %d, %d input arguments for %s function, expecting %d\n", line, num_args, get_function(func_id), 2);
-      errorOccurred = 1;
+      errorOccurred = true;
     } else if(arg_type->basic_type != Type::FLOAT && arg_type->basic_type != Type::INT && arg_type->basic_type != Type::ANY){
       fprintf(errorFile, "ERROR: line %d, incorrect argument type\n", line);
-      errorOccurred = 1;
+      errorOccurred = true;
     } else if(arg_type->length != 3 && arg_type->length != 4 && arg_type->basic_type != Type::ANY){
       fprintf(errorFile, "ERROR: line %d, incorrect argument type\n", line);
-      errorOccurred = 1;
+      errorOccurred = true;
     }
     ret_type->basic_type = arg_type->basic_type;
     ret_type->length = 1;
@@ -386,13 +386,13 @@ void function_check_args(int line, int func_id, Type *ret_type, Type *arg_type, 
     case 1: //LIT
     if(num_args != 1){
       fprintf(errorFile, "ERROR: line %d, %d input arguments for %s function, expecting %d\n", line, num_args, get_function(func_id), 1);
-      errorOccurred = 1;
+      errorOccurred = true;
     } else if(arg_type->basic_type != Type::FLOAT && arg_type->basic_type != Type::ANY){
       fprintf(errorFile, "ERROR: line %d, incorrect argument type\n", line);
-      errorOccurred = 1;
+      errorOccurred = true;
     } else if(arg_type->length != 4 && arg_type->basic_type != Type::ANY){
       fprintf(errorFile, "ERROR: line %d, incorrect argument type\n", line);
-      errorOccurred = 1;
+      errorOccurred = true;
     } 
     ret_type->basic_type = Type::FLOAT;
     ret_type->length = 4;
@@ -400,13 +400,13 @@ void function_check_args(int line, int func_id, Type *ret_type, Type *arg_type, 
     case 2: //RSQ
     if(num_args != 1){
       fprintf(errorFile, "ERROR: line %d, %d input arguments for %s function, expecting %d\n", line, num_args, get_function(func_id), 1);
-      errorOccurred = 1;
+      errorOccurred = true;
     } else if(arg_type->basic_type != Type::FLOAT && arg_type->basic_type != Type::INT && arg_type->basic_type != Type::ANY){
       fprintf(errorFile, "ERROR: line %d, incorrect argument type\n", line);
-      errorOccurred = 1;
+      errorOccurred = true;
     } else if(arg_type->length != 1 && arg_type->basic_type != Type::ANY){
       fprintf(errorFile, "ERROR: line %d, incorrect argument type\n", line);
-      errorOccurred = 1;
+      errorOccurred = true;
     }
     ret_type->basic_type = Type::FLOAT;
     ret_type->length = 1;
@@ -429,14 +429,14 @@ void unary_check_type(Type *type_out, int line, int op, Type *type_in) {
     case '!':
       if(type_in->basic_type != Type::BOOLEAN){
         fprintf(errorFile, "ERROR: line %d, argument 1 of ! operator does not have boolean type\n", line);
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       }
       break;
     case '-':
       if(!is_arithmetic(type_in)){
         fprintf(errorFile, "ERROR: line %d, argument 1 of %s operator does not have arithmetic type\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       }
       break;
@@ -466,12 +466,12 @@ void binary_check_type(Type *type_out, int line, int op, Type *type1, Type *type
     case AND:
       if(type1->basic_type != Type::BOOLEAN){
         fprintf(errorFile, "ERROR: line %d, argument 1 of %s operator does not have boolean type\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       } 
       if (type2->basic_type != Type::BOOLEAN){
         fprintf(errorFile, "ERROR: line %d, argument 2 of %s operator does not have boolean type\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       } 
       break;
@@ -488,18 +488,18 @@ void binary_check_type(Type *type_out, int line, int op, Type *type1, Type *type
     case GEQ:
       if(!is_arithmetic(type1)){
         fprintf(errorFile, "ERROR: line %d, argument 1 of %s operator does not have arithmetic type\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       } 
       if (!is_arithmetic(type2)){
         fprintf(errorFile, "ERROR: line %d, argument 2 of %s operator does not have arithmetic type\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       } 
 
       if (type1->basic_type != type2->basic_type){
         fprintf(errorFile, "ERROR: line %d, arguments of %s operator do not have same basic type\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       } 
       break;
@@ -513,7 +513,7 @@ void binary_check_type(Type *type_out, int line, int op, Type *type1, Type *type
     case AND:
       if (type1->length != type2->length){
         fprintf(errorFile, "ERROR: line %d, lengths of %s operator arguments do not match\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       }
       break;
@@ -525,7 +525,7 @@ void binary_check_type(Type *type_out, int line, int op, Type *type1, Type *type
     case '^':
       if (type1->length != type2->length){
         fprintf(errorFile, "ERROR: line %d, lengths of %s operator arguments do not match\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       }
       break;
@@ -535,14 +535,14 @@ void binary_check_type(Type *type_out, int line, int op, Type *type1, Type *type
     case GEQ:
       if (!is_scalar(type1) || !is_scalar(type2)){
         fprintf(errorFile, "ERROR: line %d, arguments of %s operator must be scalar\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       }
       break;
     case '*': 
       if (!is_scalar(type1) && !is_scalar(type2) && (type1->length != type2->length)){
         fprintf(errorFile, "ERROR: line %d, lengths of %s operator vector arguments do not match\n", line, get_operator(op));
-        errorOccurred = 1;
+        errorOccurred = true;
         return;
       }
       break;
