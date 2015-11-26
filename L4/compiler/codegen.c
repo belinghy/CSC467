5d26959bbb3ae0b59ccd241b142863bd2a8a4514
tina <tina.han.tiantian@gmail.com>
2015-11-26 14:17:36 -0500

    added code gen files

---
+++ L4/compiler/codegen.c
@@ -0,0 +1,5 @@
+#include "codegen.h"
+
+void genCode(node *ast){
+
+}
---
+++ L4/compiler/codegen.h
@@ -0,0 +1,13 @@
+#ifndef _CODEGEN
+#define _CODEGEN 1
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "common.h"
+#include "ast.h"
+
+void genCode(node *ast);
+
+#endif
